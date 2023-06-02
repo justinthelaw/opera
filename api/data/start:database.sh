0a23e462e8704f61526545265a6850e806b520d1
jlaw <justin.law.ctr@km.spaceforce.mil>
2023-06-02 09:25:53 -0700

    Build(Database): Moved volumes directory in script

--- api/data/start:database.sh
+++ api/data/start:database.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm --name mongodb -v $PWD/server/data/db:/data/db -p $(awk -F "=" '/MONGO_PORT/{print $NF}' ./config/.env.local):27017 --env-file $PWD/config/.env.local mongodb/mongodb-community-server:latest
+docker run --rm --name mongodb -v $PWD/api/data/db:/data/db -p $(awk -F "=" '/MONGO_PORT/{print $NF}' ./config/.env.local):27017 --env-file $PWD/config/.env.local mongodb/mongodb-community-server:latest
