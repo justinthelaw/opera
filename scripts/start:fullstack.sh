53b152079fe18d9c744005331f752da9d408781b
jlaw <justin.law.ctr@km.spaceforce.mil>
2023-06-02 09:00:37 -0700

    Ops(Fullstack): Improved start:fullstack, added stop:fullstack

--- scripts/start:fullstack.sh
+++ scripts/start:fullstack.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
 
-echo "==> Smarter Bullets Development Environment is spinning up...";
+# Function to execute when Ctrl+C is pressed
+cleanup() {
+    echo "==> Running Smarter Bullets cleanup..."
+    npm run stop:fullstack
+    exit 0
+}
 
-npm run install:all && source ./config/.env.local && \
+# Register the cleanup function to be called on SIGINT (Ctrl+C)
+trap cleanup SIGINT
 
-if ! nc -z localhost $MONGO_PORT; then
-    npm run start:database;
-fi && \
+echo "==> Smarter Bullets Development Environment is spinning up..."
 
-if nc -z localhost $MONGO_PORT && ! nc -z localhost $API_PORT; then
-    npm run start:api;
-fi && \
+source ./config/.env.local
 
-if ! nc -z localhost $API_PORT && ! nc -z localhost $CLIENT_PORT; then
-    npm run start:client;
-fi && \
+# Start the processes in the foreground
+npm run start:database &
+database_pid=$!
 
-echo "==> Smarter Bullets Development Environment is up!";
-echo "==> Logs for all sub-stacks will display below as they are written...";
+while ! nc -z localhost $MONGO_PORT; do sleep 1; done
+
+npm run start:api &
+api_pid=$!
+
+while ! nc -z localhost $API_PORT; do sleep 1; done
+
+npm run start:client &
+client_pid=$!
+
+# Wait for all processes to finish
+wait $database_pid
+wait $api_pid
+wait $client_pid
+
+echo "==> Smarter Bullets Development Environment is up and running!"
+echo "==> Logs for all sub-stacks will display in this terminal as they are written."
