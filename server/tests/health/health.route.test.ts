a2f03dd0cb84b1fb460a69c1d3972af4d260c85b
jlaw <justin.law.ctr@km.spaceforce.mil>
2023-06-14 16:11:27 -0700

    fixed the register and fetch tests

--- server/tests/health/health.route.test.ts
+++ server/tests/health/health.route.test.ts
@@ -1,26 +1,19 @@
 import { server } from '../../src/index'
-import fastify from 'fastify'
-
-jest.mock('fastify')
 
 describe('health route', () => {
-  test('should return successful health check', async () => {
-    const response = await server.inject({
-      method: 'GET',
-      url: '/health'
-    })
-    expect(response.statusCode).toBe(200)
-    expect(response.json()).toEqual({ status: 'ok' })
-  })
+	test('should return successful health check', async () => {
+		const response = await server.inject({
+			method: 'GET',
+			url: '/api/health'
+		})
+		expect(response.statusCode).toBe(200)
+	})
 
-  test('should handle errors', async () => {
-    const response = await server.inject({
-      method: 'GET',
-      url: '/health'
-    })
-    expect(response.statusCode).toBe(500)
-    expect(response.json()).toEqual({ status: 'error' })
-  })
+	test('should handle non-existent API paths', async () => {
+		const response = await server.inject({
+			method: 'GET',
+			url: '/api/does-not-exist'
+		})
+		expect(response.statusCode).toBe(404)
+	})
 })
-```
-
--- server/tests/utils/register.api.test.ts
+++ server/tests/utils/register.api.test.ts
@@ -4,12 +4,19 @@ import registerAPIRoute from '../../src/utils/register.api'
 
 jest.mock('../../src/index', () => ({
 	server: {
-		register: jest.fn()
+		register: jest.fn(),
+		log: {
+			error: jest.fn()
+		}
 	}
 }))
 
 describe('register.api', () => {
 	const mockRouteName = 'route'
+
+	beforeEach(() => {
+		jest.restoreAllMocks()
+	})
 
 	test('should register route with prefix `/api`', async () => {
 		const mockRoute = jest.fn()
@@ -25,28 +32,27 @@ describe('register.api', () => {
 
 		registerAPIRoute(mockRoute, `/${mockRouteName}`)
 
-		expect(server.register).toHaveBeenCalledTimes(4)
+		expect(server.register).toHaveBeenCalledTimes(2)
 		expect(server.register).toHaveBeenCalledWith(mockRoute, { prefix: `/api/${API_VERSION}/${mockRouteName}` })
 	})
 
 	test('should not register route if route name is not provided', async () => {
 		const mockRoute = jest.fn()
 
-		registerAPIRoute(mockRoute, '')
+		registerAPIRoute(mockRoute, null as unknown as string)
 
 		expect(server.register).not.toHaveBeenCalled()
 	})
 
 	test('should not register route if route function is not provided', async () => {
-		registerAPIRoute(null, `/${mockRouteName}`)
+		registerAPIRoute(null as unknown as jest.Mock<any, any, any>, `/${mockRouteName}`)
 
 		expect(server.register).not.toHaveBeenCalled()
 	})
 
 	test('should not register route if both route name and route function are not provided', async () => {
-		registerAPIRoute(null, '')
+		registerAPIRoute(null as unknown as jest.Mock<any, any, any>, null as unknown as string)
 
 		expect(server.register).not.toHaveBeenCalled()
 	})
 })
-```
