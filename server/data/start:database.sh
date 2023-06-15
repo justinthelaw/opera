a3d73cf1726dd36451183a0861b5d48ebf261586
jlaw <justin.law.ctr@km.spaceforce.mil>
2023-06-15 11:43:03 -0700

    database docker pull changed to mongo:latest

--- server/data/start:database.sh
+++ server/data/start:database.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ENV=/config/.env.local
-docker run --rm --name mongodb -v $PWD/server/data/db:/data/db -p $(awk -F "=" '/MONGO_PORT/{print $NF}' .$ENV):27017 --env-file $PWD/$ENV mongodb/mongodb-community-server:latest
+docker run --rm --name mongodb -v $PWD/server/data/db:/data/db -p $(awk -F "=" '/MONGO_PORT/{print $NF}' .$ENV):27017 --env-file $PWD/$ENV mongo:latest
 
