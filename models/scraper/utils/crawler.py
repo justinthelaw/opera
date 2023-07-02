164bd148250e08ad2a277b17b62d799a05001ccc
Justin Law <justin.law.ctr@km-sscdev.org>
2023-07-02 13:41:21 -0700

    Fix(BulletForge): Scraper and ipynb working for eprbullets

--- models/scraper/utils/crawler.py
+++ models/scraper/utils/crawler.py
@@ -7,22 +7,28 @@ from loguru import logger
 # Track visited URLs to avoid duplicates
 visited_urls = set()
 
+def has_base_url(base_url, url):
+    parsed_base_url = urlparse(base_url)
+    parsed_url = urlparse(url)
+
+    if parsed_url.netloc != parsed_base_url.netloc:
+        # Skip URLs outside the base URL
+        return False
+    
+    return True
+
 # Crawl and scrape a page based on the bullet regex pattern
-async def crawl_level_1(base_url, url, file_path):
+async def crawl(base_url, url, file_path):
+    confirmed_base_url = has_base_url(base_url, url)
+    if not confirmed_base_url:
+        logger.warning(f"Skipping out-of-scope URL: {url}")
+        visited_urls.remove(url)
+        return
     try:
-        if not url.startswith(('http://', 'https://')):
-            # Resolve relative URLs
-            url = urljoin(base_url, url)
-
-        parsed_base_url = urlparse(base_url)
-        parsed_url = urlparse(url)
-
-        if parsed_url.netloc != parsed_base_url.netloc:
-            return  # Skip URLs outside the base URL
-
         async with httpx.AsyncClient() as client:
             # Send an async GET request to fetch the web page content
-            response = await client.get(url)
+            response = await client.get(url, headers={"User-Agent": "Mozilla/5.0"})
+            # Raise exception related to response
             response.raise_for_status()
             page_content = response.text
 
@@ -40,13 +46,14 @@ async def crawl_level_1(base_url, url, file_path):
             for content in relevant_content:
                 file.write(content + "\n")
 
+        logger.success(f"Page #{len(visited_urls)}: {url}")
+
         # Find all links on the page and crawl them recursively
         for link in soup.find_all('a', href=True):
             absolute_link = urljoin(url, link['href'])
             if absolute_link not in visited_urls:
                 visited_urls.add(absolute_link)
-                await crawl_level_1(base_url, absolute_link, file_path)
-                logger.success(f"\tPage #{len(visited_urls)}: {absolute_link}")
+                await crawl(base_url, absolute_link, file_path)
 
     # Error handling for the entire crawl process
     except httpx.RequestError as e:
---
+++ models/scraper/utils/file_utils.py
@@ -0,0 +1,28 @@
+import os
+from loguru import logger
+
+# Warns for overwriting an existing file, then deletes file
+def delete_file(file_path):
+    try:
+        if os.path.exists(file_path):
+            os.remove(file_path)
+            logger.warning(f"File '{file_path}' already existed and has been deleted")
+        logger.info(f"Printing all matches to file path: {file_path}")
+    except Exception as e:
+        logger.error(f"A runtime error occurred: {e}")
+
+# Cleans extra new lines in file
+def clean_file(file_path):
+    try:
+        logger.info("Cleaning up output file...")
+        # Read the file and filter out empty lines
+        with open(file_path, "r") as file:
+            lines = file.readlines()
+            lines = [line.strip() for line in lines if line.strip()]
+
+        # Write the filtered lines back to the file
+        with open(file_path, "w") as file:
+            file.write("\n".join(lines))
+
+    except Exception as e:
+        logger.error(f"A runtime error occurred: {e}")
