5948c8c2e8affab510727049ce7a53464a8c5a31
Alden Davidson <gitlab.washout767@simplelogin.com>
2023-07-12 21:03:08 -0700

    Configure Prettier and ESLint

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	preset: 'ts-jest',
-	testEnvironment: 'jsdom',
-	setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect']
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect']
 }
--- client/tests/App.test.tsx
+++ client/tests/App.test.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
 import { render } from '@testing-library/react'
+import React from 'react'
 import App from '../src/App'
 
 describe('App', () => {
-	test('should render the Home page', () => {
-		const { getByTestId } = render(<App />)
-		const homePage = getByTestId('home.page')
-		expect(homePage).toBeInTheDocument()
-	})
+    test('should render the Home page', () => {
+        const { getByTestId } = render(<App />)
+        const homePage = getByTestId('home.page')
+        expect(homePage).toBeInTheDocument()
+    })
 })
