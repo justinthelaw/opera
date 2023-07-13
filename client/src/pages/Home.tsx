61b0e6af64894b1a64d26115ad1d91a2a174f8fb
Alden Davidson <gitlab.washout767@simplelogin.com>
2023-07-12 22:28:43 -0700

    Adjust Home.tsx spacing to 2 spaces

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Home: React.FC = () => {
-    return (
-        <div data-testid='home.page'>
-            <h1>Smarter Bullets</h1>
-        </div>
-    )
+  return (
+    <div data-testid='home.page'>
+      <h1>Smarter Bullets</h1>
+    </div>
+  )
 }
 
 export default Home
