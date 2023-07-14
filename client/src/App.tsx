9b348b23a936d7df3691234c16abbdb25069f342
Alden Davidson <gitlab.washout767@simplelogin.com>
2023-07-13 21:28:56 -0700

    Restore root package.json and move linting commands into client package.json

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 
 const App: React.FC = () => {
