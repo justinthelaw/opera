e9e8434bb8ff8323d45b6b3aee17e7d224da81c0
Justin Law <justin.law.ctr@km-sscdev.org>
2023-07-15 22:42:03 -0700

    Feature(BulletForge): Increased Fine Tune Control

--- forge/notebooks/scripts/bullet_patterns.py
+++ forge/notebooks/scripts/bullet_patterns.py
@@ -1,4 +1,4 @@
-# This is the regex for a standard bullet pattern "-<ACTION>;<IMPACT>--<OUTCOME>"
+# This is the regex for a standard bullet pattern "-[ACTION];[IMPACT]--[OUTCOME]"
 # The pattern is not all-encompassing - this hardcoded value can to be experimented with
 # to achieve maximum bullet capture
 BULLET_PATTERN = r"^-?([\w\W\s]{0,255});?([\w\W\s]{0,255})--([\w\W\s]{0,255})$"
