986b0f126ecbe46a2df1e1f287f4fb1e2eb1f610
Justin Law <justin.law.ctr@km-sscdev.org>
2023-07-16 15:39:45 -0700

    Fix(BulletForge): fix rich logger, repeat loss

--- forge/notebooks/scripts/rich_logger.py
+++ forge/notebooks/scripts/rich_logger.py
@@ -1,36 +1,42 @@
 from rich.table import Column, Table
-from rich import box
 from rich.console import Console
 from rich.live import Live
 
-
+# Training table class
 class TrainingTable:
     def __init__(self):
         self.table = Table(
             Column("Epoch", justify="left"),
-            Column("Steps", justify="left"),
-            Column("Loss, Calculator", justify="left"),
+            Column("Loss Rate", justify="left"),
             title="Training Status",
-            padding=2,
-            expand=True,
-            pad_edge=True,
-            box=box.ROUNDED,
         )
+        self.epoch_refresh = True
 
-    def add_row(self, epoch, steps, loss):
-        # Add row to the table
-        self.table.add_row(str(epoch + 1), str(steps + 1), str(loss))
+    def add_row(self, epoch, loss):
+        self.table.add_row(str(epoch + 1), str(loss))
 
     def get_table(self):
-        # Return current state of the table
         return self.table
+
+    def get_epoch_refresh(self):
+        return self.epoch_refresh
+
+    def refresh_table(self, epoch, total_loss):
+        self.add_row(epoch, total_loss)
+        live_refresher.update(training_table.get_table(), refresh=True)
+        self.epoch_refresh = False
+
+    def switch_epoch_refresh(self):
+        self.epoch_refresh = True
 
 
 # Define a rich console logger
-general_logger = Console(record=False, highlight=False)
+general_logger = Console(record=False)
 
 # Instantiate new training table
 training_table = TrainingTable()
 
 # Generate new live logging instance
-live_refresher = Live(training_table, console=general_logger, auto_refresh=False)
+live_refresher = Live(
+    training_table.get_table(), console=general_logger, auto_refresh=False
+)
