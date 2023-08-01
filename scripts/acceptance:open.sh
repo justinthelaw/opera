999ee99bb5b05f7976a42a3c241f905fde269f21
Justin Law <justin.law.ctr@km-sscdev.org>
2023-08-01 11:29:10 -0400

    fixed linting, scripts for cypress

--- scripts/acceptance:open.sh
+++ scripts/acceptance:open.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo -ne "==> Running acceptance tests in open mode...\n"
+echo -ne "> Running acceptance tests in open mode...\n"
 
 $(npm run start:all) </dev/null &>/dev/null & \
 
 source ./config/.env.local && \
 
 # Wait for the CLIENT to be accessible
-while ! nc -z localhost $CLIENT_PORT; do echo "==> Waiting on Client..." && sleep 3; done
+while ! nc -z localhost $CLIENT_PORT; do echo "> Waiting on Client..." && sleep 3; done
 
 cd acceptance && npm run cypress:open && \
 
-echo -ne "==> Acceptance tests complete!\n"
+echo -ne "> Acceptance tests complete!\n"
