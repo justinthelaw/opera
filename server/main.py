023caaafd691a852f635f15922599f614079431c
Justin Law <justin.law.ctr@km-sscdev.org>
2023-08-07 14:18:41 -0700

    fixed start script, config

--- server/main.py
+++ server/main.py
@@ -8,4 +8,4 @@ load_dotenv("../config/.env.local")
 host = os.getenv("SERVER_HOST")
 port = int(os.getenv("SERVER_PORT"))
 
-run("main:app", host=host, port=port)
+run("server:app", host=host, port=port)
