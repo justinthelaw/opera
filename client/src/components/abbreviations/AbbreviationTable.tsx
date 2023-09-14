d4e7fd334f013d96b0ef1c54cba01cee2dbdaa2b
justinthelaw <justinwingchunglaw@gmail.com>
2023-09-14 15:35:45 -0700

    Fix(Build): Tests and pipeline bandages

--- client/src/components/abbreviations/AbbreviationTable.tsx
+++ client/src/components/abbreviations/AbbreviationTable.tsx
@@ -64,7 +64,7 @@ export const AbbreviationTable = ({ data, setData }: Props) => {
                 }
             })
 
-            setData(newData ? newData : [])
+            setData(newData || [])
         }
     }
 
